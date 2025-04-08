f34a2246b00770c2b5321dc2a939a02e6fd00dab
Mateo Herrera <mateo.herrera@udla.edu.ec>
2025-04-08 09:00:59 -0500

    controller

--- Taller-LigaPro/Models/ErrorViewModel.cs
+++ Taller-LigaPro/Models/ErrorViewModel.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Taller_LigaPro.Models
 {
     public class ErrorViewModel
@@ -5,5 +7,23 @@ namespace Taller_LigaPro.Models
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+    public class Equipo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Nombre { get; set; }
+        [Range(0,20)]
+        public int PartidosJugados { get; set; }
+        [Range(0, 20)]
+        public int PartidosGanadados { get; set; }
+        [Range(0, 20)]
+        public int PartidosEmpatados { get; set; }
+        [Range(0, 20)]
+        public int PartidosPerdidos { get; set; }
+        public int TotalPuntos { get; set;}
+
     }
 }
